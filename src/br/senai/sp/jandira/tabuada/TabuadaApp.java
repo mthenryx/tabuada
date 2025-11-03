21438af79a65955469257d38fa0e1ac38864d3a3
MATHEUS HENRY DOS SANTOS <25204051@CFP127ALUNO.LOCAL>
2025-11-03 16:57:14 -0300

    Finalizada a tabuada, foram usados loop e vetor

--- src/br/senai/sp/jandira/tabuada/TabuadaApp.java
+++ src/br/senai/sp/jandira/tabuada/TabuadaApp.java
@@ -1,4 +1,12 @@
 package br.senai.sp.jandira.tabuada;
 
+import br.senai.sp.jandira.tabuada.model.Tabuada;
+
 public class TabuadaApp {
+    public static void main(String[] args) {
+        System.out.println("Executando Programa...");
+
+        Tabuada tabuada = new Tabuada();
+        tabuada.obterDados();
+    }
 }
--- src/br/senai/sp/jandira/tabuada/model/Tabuada.java
+++ src/br/senai/sp/jandira/tabuada/model/Tabuada.java
@@ -1,4 +1,66 @@
 package br.senai.sp.jandira.tabuada.model;
 
+import java.util.Scanner;
+
 public class Tabuada {
+
+    int multiplicando;
+    int multiplicadorInicial;
+    int multiplicadorFinal;
+    String [] tabuada;
+
+    public void obterDados() {
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.print("Qual o valor do multiplicando? ");
+        multiplicando = leitor.nextInt();
+
+        System.out.print("Qual o valor do multiplicador inicial? ");
+        multiplicadorInicial = leitor.nextInt();
+
+        System.out.print("Qual o valor do multiplicador final? ");
+        multiplicadorFinal = leitor.nextInt();
+
+        corrigirOrdemValores();
+    }
+
+    public void corrigirOrdemValores() {
+        int troca;
+
+        if (multiplicadorInicial > multiplicadorFinal) {
+            troca = multiplicadorInicial;
+            multiplicadorInicial = multiplicadorFinal;
+            multiplicadorFinal = troca;
+        }
+
+        calcularTabuada();
+    }
+
+    public void calcularTabuada() {
+        int tamanho = multiplicadorFinal - multiplicadorInicial + 1;
+        tabuada = new String[tamanho];
+
+        int i = 0;
+        while (i < tamanho) {
+            int produto = multiplicando * multiplicadorInicial;
+            tabuada[i] = multiplicando + " x " + multiplicadorInicial + " = " + produto;
+            multiplicadorInicial = multiplicadorInicial + 1;
+            i++;
+        }
+
+        exibirTabuada();
+    }
+
+    public void exibirTabuada() {
+        System.out.println("---------------------------------");
+        System.out.println("Resultado da sua tabuada");
+
+        int i = 0;
+        while (i < tabuada.length) {
+            System.out.println(tabuada[i]);
+            i++; //i = i + 1
+        }
+
+        System.out.println("---------------------------------");
+    }
 }
